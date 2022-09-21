881eb891ff12ed9ecb6488ca730fb7a8dec4112f
martuafernando <fernandosibarani45@gmail.com>
2022-09-21 23:54:02 +0700

    add header text and search field

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/default.css';
 
+import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/js/solid';
+
 console.log('Hello Coders! :)');
