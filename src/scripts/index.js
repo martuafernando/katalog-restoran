25ff298187ce51f3d75c1c3b112c60eb62b5a824
martuafernando <fernandosibarani45@gmail.com>
2022-09-22 23:46:23 +0700

    add service, list retaurant, footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,5 +4,6 @@ import '../styles/default.css';
 
 import '@fortawesome/fontawesome-free';
 import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/brands';
 
 console.log('Hello Coders! :)');
